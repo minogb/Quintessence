a83ee77e593b988e47c8d0a74ca1e5e6553c9cd9
minogb <minoguebrad@rocketamil.com>
2019-01-05 15:35:36 -0800

    checkpoint

---
+++ Source/Quint/Item.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Item.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUINT_API UItem : public UObject
+{
+	GENERATED_BODY()
+	
+	
+};
---
+++ Source/Quint/Weapon.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Equipment.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUINT_API UWeapon : public UEquipment
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
